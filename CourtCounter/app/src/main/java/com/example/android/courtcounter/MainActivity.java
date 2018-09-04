b025d078f2ae52a7228a3e554bdafa0e238ac4a3
Gustavo Azevedo <gustavo_azevedo_7@hotmail.com>
2018-09-04 09:27:41 -0300

    Counter for both teams & reset button

--- CourtCounter/app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ CourtCounter/app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -8,31 +8,61 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     int scoreA = 0;
+    int scoreB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         displayForTeamA(scoreA);
+        displayForTeamB(scoreB);
     }
 
     public void plus3Points(View v) {
-        scoreA = scoreA + 3;
-        displayForTeamA(scoreA);
+        if (v.getTag().toString().equals("team_a")) {
+            scoreA = scoreA + 3;
+            displayForTeamA(scoreA);
+        } else if (v.getTag().toString().equals("team_b")) {
+            scoreB = scoreB + 3;
+            displayForTeamB(scoreB);
+        }
     }
 
     public void plus2Points(View v) {
-        scoreA = scoreA + 2;
-        displayForTeamA(scoreA);
+        if (v.getTag().toString().equals("team_a")) {
+            scoreA = scoreA + 2;
+            displayForTeamA(scoreA);
+        } else if (v.getTag().toString().equals("team_b")) {
+            scoreB = scoreB + 2;
+            displayForTeamB(scoreB);
+        }
     }
 
     public void plus1Point(View v) {
-        scoreA = scoreA + 1;
-        displayForTeamA(scoreA);
+        if (v.getTag().toString().equals("team_a")) {
+            scoreA = scoreA + 1;
+            displayForTeamA(scoreA);
+        } else if (v.getTag().toString().equals("team_b")) {
+            scoreB = scoreB + 1;
+            displayForTeamB(scoreB);
+        }
     }
 
     public void displayForTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
         scoreView.setText(String.valueOf(score));
     }
+
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void resetCounter(View v) {
+        scoreA = 0;
+        scoreB = 0;
+        displayForTeamA(scoreA);
+        displayForTeamB(scoreB);
+    }
+
 }
