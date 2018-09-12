4604470e18aa109ae9a1793a16f154b1cc937f00
Gustavo Azevedo <gustavo_azevedo_7@hotmail.com>
2018-09-12 17:54:11 -0300

    Send order by mail

--- JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -10,6 +10,8 @@ package com.example.android.justjava;
 
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -41,7 +43,14 @@ public class MainActivity extends AppCompatActivity {
         EditText nameField = (EditText) findViewById(R.id.name);
         String name = nameField.getText().toString();
         String orderSummaryMessage = createOrderSummary(quantity, price, name);
-        displayMessage(orderSummaryMessage);
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:"));
+        intent.putExtra(Intent.EXTRA_SUBJECT, "JustJava Coffee Order to " + name);
+        intent.putExtra(Intent.EXTRA_TEXT, orderSummaryMessage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 
     private int calculatePrice(int quantity) {
@@ -60,7 +69,7 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "Limit of 100 units per order.", Toast.LENGTH_SHORT).show();
             return;
         }
-        quantity = quantity + 1;
+        quantity += 1;
         displayQuantity(quantity);
         int price = calculatePrice(quantity);
         displayPrice(price);
@@ -71,7 +80,7 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "You cannot have less than 1 coffee. ", Toast.LENGTH_SHORT).show();
             return;
         }
-        quantity = quantity - 1;
+        quantity -= 1;
         displayQuantity(quantity);
         int price = calculatePrice(quantity);
         displayPrice(price);
