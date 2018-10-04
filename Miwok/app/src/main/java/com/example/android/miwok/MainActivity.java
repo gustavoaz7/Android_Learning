5d50c73f0ef3a90705357fc1b6dc867e5b5dcdb0
Gustavo Azevedo <gustavo_azevedo_7@hotmail.com>
2018-10-04 09:29:55 -0300

    New clickable activities

--- Miwok/app/src/main/java/com/example/android/miwok/MainActivity.java
+++ Miwok/app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.android.miwok;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView numbers = (TextView) findViewById(R.id.numbers);
+        numbers.setOnClickListener(new NumbersClickListener());
+    }
+
+    public void openNumbersList(View v) {
+        Intent i = new Intent(this, NumbersActivity.class);
+        startActivity(i);
+    }
+
+    public void openPhrasesList(View v) {
+        Intent i = new Intent(this, PhrasesActivity.class);
+        startActivity(i);
+    }
+
+    public void openColorsList(View v) {
+        Intent i = new Intent(this, ColorsActivity.class);
+        startActivity(i);
+    }
+
+    public void openFamilyList(View v) {
+        Intent i = new Intent(this, FamilyActivity.class);
+        startActivity(i);
     }
 }
