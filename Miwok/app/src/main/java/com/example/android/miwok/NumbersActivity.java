e9f173c1d5d6632a1ae04b8d0aa97aaeef54c6aa
Gustavo Azevedo <gustavo_azevedo_7@hotmail.com>
2018-10-10 09:44:18 -0300

    Generalize list & add for all categories

--- Miwok/app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ Miwok/app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,7 +2,6 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -12,7 +11,7 @@ public class NumbersActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_numbers);
+        setContentView(R.layout.list);
 
         ArrayList<Word> words = new ArrayList<Word>();
         words.add(new Word("one", "lutti"));
@@ -27,7 +26,7 @@ public class NumbersActivity extends AppCompatActivity {
         words.add(new Word("ten", "na’aacha"));
 
          WordAdapter adapter = new WordAdapter(this, words);
-         ListView listView = (ListView) findViewById(R.id.numbers_list);
+         ListView listView = (ListView) findViewById(R.id.list);
          listView.setAdapter(adapter);
     }
 
