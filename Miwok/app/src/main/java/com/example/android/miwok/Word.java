eb865f1bc25155e2027c3df1a7b3855cd55810b0
Gustavo Azevedo <gustavo_azevedo_7@hotmail.com>
2018-10-17 09:05:37 -0300

    Add images to list items

--- Miwok/app/src/main/java/com/example/android/miwok/Word.java
+++ Miwok/app/src/main/java/com/example/android/miwok/Word.java
@@ -2,12 +2,20 @@ package com.example.android.miwok;
 
 public class Word {
 
+    private static final int NO_IMAGE = -1;
     private String mDefault;
     private String mMiwok;
+    private int mImageResourceId = NO_IMAGE;
 
     public Word(String defaultWord, String miwokWord) {
         mDefault = defaultWord;
         mMiwok = miwokWord;
+    }
+
+    public Word(String defaultWord, String miwokWord, int imageResourceId) {
+        mDefault = defaultWord;
+        mMiwok = miwokWord;
+        mImageResourceId = imageResourceId;
     }
 
     public String getDefault() {
@@ -16,5 +24,13 @@ public class Word {
 
     public String getMiwok() {
         return mMiwok;
+    }
+
+    public int getImageResourceId() {
+        return mImageResourceId;
+    }
+
+    public boolean hasImage() {
+        return mImageResourceId != NO_IMAGE;
     }
 }
--- Miwok/app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ Miwok/app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -36,6 +37,14 @@ public class WordAdapter extends ArrayAdapter<Word> {
 
         TextView defaultText = (TextView) listItem.findViewById(R.id.default_text);
         defaultText.setText(currentWord.getDefault());
+
+        ImageView image = (ImageView) listItem.findViewById(R.id.image);
+        if (currentWord.hasImage()) {
+            image.setImageResource(currentWord.getImageResourceId());
+            image.setVisibility(View.VISIBLE);
+        } else {
+            image.setVisibility(View.GONE);
+        }
 
 
         return listItem;
