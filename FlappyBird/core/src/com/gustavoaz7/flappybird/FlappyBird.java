d863783710e99a6b394420d0b0f1993110354f5e
gustavoaz7 <gustavo_azevedo_7@hotmail.com>
2019-03-16 09:43:14 -0300

    Add pipe & score

--- FlappyBird/core/src/com/gustavoaz7/flappybird/FlappyBird.java
+++ FlappyBird/core/src/com/gustavoaz7/flappybird/FlappyBird.java
@@ -2,24 +2,38 @@ package com.gustavoaz7.flappybird;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import java.util.Random;
 
 public class FlappyBird extends ApplicationAdapter {
 	private SpriteBatch batch;
 	private Texture[] birds;
 	private Texture background;
+	private Texture pipeBottom;
+	private Texture pipeTop;
+	private Random rand;
+	private BitmapFont font;
 
 	private int screenWidth;
 	private int screenHeight;
 	private int move = 0;
 	private int fallSpeed = 0;
-	private int x;
-	private int y;
+	private int birdX;
+	private int birdY;
+	private int pipeX;
+	private int passageHeight;
+	private int passagePositionY;
+	private int score = 0;
 
-	private float delta = 0;
+	private float index = 0;
+	private float deltaTime;
 
+	private boolean gameStarted = false;
+	private boolean scored = false;
 
 	@Override
 	public void create () {
@@ -29,29 +43,67 @@ public class FlappyBird extends ApplicationAdapter {
 		birds[1] = new Texture("bird2.png");
 		birds[2] = new Texture("bird3.png");
 		background = new Texture("background.png");
+		pipeBottom = new Texture("pipe_bottom_lg.png");
+		pipeTop = new Texture("pipe_top_lg.png");
+		rand = new Random();
+		font = new BitmapFont();
+		font.setColor(Color.WHITE);
+		font.getData().setScale(6);
 		screenWidth = Gdx.graphics.getWidth();
 		screenHeight = Gdx.graphics.getHeight();
-		x = screenWidth / 2 - 50;
-		y = screenHeight / 2 -50;
+		birdX = (int) (screenWidth * 0.2);
+		birdY = screenHeight / 2 -50;
+		pipeX = screenWidth;
+		passageHeight = 300;
 	}
 
 	@Override
 	public void render () {
-		delta += Gdx.graphics.getDeltaTime() * 3;
-		if (delta >= 3) {
-			delta = 0;
+		deltaTime = Gdx.graphics.getDeltaTime();
+
+		index += deltaTime * 9;
+		if (index >= 3) {
+			index = 0;
 		}
 
-		if (Gdx.input.justTouched()) {
-			fallSpeed = -20;
-		}
+		if (!gameStarted) {
+			if (Gdx.input.justTouched()) {
+				gameStarted = true;
+			}
+		} else {
+			fallSpeed++;
+			pipeX -= deltaTime * 600;
 
-		fallSpeed++;
-		y -= fallSpeed;
+
+
+			if (Gdx.input.justTouched()) {
+				fallSpeed = -20;
+			}
+
+			if (birdY > 0) {
+				birdY -= fallSpeed;
+			}
+
+			if (pipeX < -pipeBottom.getWidth()) {
+				pipeX = screenWidth;
+				passagePositionY = rand.nextInt(400) - 200;
+				scored = false;
+			}
+
+			if (pipeX < birdX) {
+				if (!scored) {
+					score++;
+					scored = true;
+				}
+			}
+		}
 
 		batch.begin();
 		batch.draw(background, 0, 0, screenWidth, screenHeight);
-		batch.draw(birds[(int) delta], x, y);
+		batch.draw(pipeTop, pipeX, screenHeight / 2 + passageHeight / 2 + passagePositionY);
+		batch.draw(pipeBottom, pipeX, 0 - passageHeight / 2 + passagePositionY);
+		batch.draw(birds[(int) index], birdX, birdY);
+		font.draw(batch, String.valueOf(score), screenWidth / 2, (int) (screenHeight - screenHeight * 0.1));
 		batch.end();
 	}
 
